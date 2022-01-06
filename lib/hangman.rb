d5f68f36256349cd6d6221885e4ee58867b03727
ATDillon <vieridin@gmail.com>
2022-01-06 15:08:58 -0800

    Add method to pick a random word

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,4 +1,18 @@
 # Holds script for the game hangman
 class Hangman
-  def initialize; end
+  attr_accessor :word
+  attr_reader :player, :dictionary
+
+  def initialize(player:)
+    @player = player
+    @dictionary = File.readlines('5desk.txt')
+    @word = word_picker
+  end
+
+  def word_picker
+    loop do
+      new_word = dictionary[Random.rand(dictionary.length)]
+      break new_word unless new_word.length < 5 || new_word.length > 12
+    end
+  end
 end
