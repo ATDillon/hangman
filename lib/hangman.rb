577770cbc77d6b7abaafb8e27c8e1f75994758c0
ATDillon <vieridin@gmail.com>
2022-01-12 18:55:45 -0800

    Add method for word display between turns

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,6 +1,11 @@
 # Holds script for the game hangman
 class Hangman
+  attr_reader :guessed
+
+  private
+
   attr_accessor :word
+  attr_writer :guessed
   attr_reader :player, :dictionary
 
   def initialize(player:)
@@ -12,8 +17,23 @@ class Hangman
 
   def word_picker
     loop do
-      new_word = dictionary[Random.rand(dictionary.length)]
+      new_word = dictionary[Random.rand(dictionary.length)].chomp
       break new_word unless new_word.length < 5 || new_word.length > 12
     end
+  end
+
+  def letter_hider(string)
+    result = string.split('').map do |letter|
+      if guessed.include?(letter.downcase)
+        letter
+      else
+        '-'
+      end
+    end
+    result.join('')
+  end
+
+  def word_display(string)
+    puts letter_hider(string)
   end
 end
